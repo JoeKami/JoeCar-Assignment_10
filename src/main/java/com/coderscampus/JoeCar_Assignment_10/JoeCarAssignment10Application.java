c9afe0f2bc428e8d34cbae3633d8c32871788afb
JoeKami <j.b.carrasquillo@gmail.com>
2024-10-16 23:16:52 -0400

    Initial commit

---
+++ src/main/java/com/coderscampus/JoeCar_Assignment_10/JoeCarAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.JoeCar_Assignment_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeCarAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeCarAssignment10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/JoeCar_Assignment_10/JoeCarAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.JoeCar_Assignment_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeCarAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
