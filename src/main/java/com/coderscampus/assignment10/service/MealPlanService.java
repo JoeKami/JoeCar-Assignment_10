4e4e006bb2d9a4e7a309f3dd4f8a29b8d993ee60
JoeKami <j.b.carrasquillo@gmail.com>
2024-10-17 14:48:36 -0400

    Updated MealPlanService to properly set Param values
    accurately returns data retrieved from the Get request to the API back to the browser/query.

--- src/main/java/com/coderscampus/assignment10/service/MealPlanService.java
+++ src/main/java/com/coderscampus/assignment10/service/MealPlanService.java
@@ -4,7 +4,6 @@ package com.coderscampus.assignment10.service;
 import com.coderscampus.assignment10.domain.DayMeals;
 import com.coderscampus.assignment10.domain.WeekMeals;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -39,14 +38,13 @@ public class MealPlanService {
                 .queryParam("apiKey", apiKey);
 
         String finalUrl = uriComponentsBuilder.toUriString();
-
         ResponseEntity<DayMeals> response = restTemplate.getForEntity(finalUrl, DayMeals.class);
 
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return response.getBody();
-        } else {
-            throw new RuntimeException("Failed to fetch daily meals: " + response.getStatusCode());
-        }
+        DayMeals dayMeals = new DayMeals();
+        dayMeals.setDiet(diet);
+        dayMeals.setNumCalories(numCalories);
+        dayMeals.setExclusions(exclusions);
+        return dayMeals;
     }
 
     public WeekMeals getWeeklyMeals(String numCalories, String diet, String exclusions) {
@@ -60,13 +58,16 @@ public class MealPlanService {
                 .queryParam("apiKey", apiKey);
 
         String finalUrl = uriComponentsBuilder.toUriString();
-
         ResponseEntity<WeekMeals> response = restTemplate.getForEntity(finalUrl, WeekMeals.class);
 
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return response.getBody();
-        } else {
-            throw new RuntimeException("Failed to fetch daily meals: " + response.getStatusCode());
-        }
+        WeekMeals weekMeals = new WeekMeals();
+        weekMeals.setNumCalories(numCalories);
+        weekMeals.setDiet(diet);
+        weekMeals.setExclusions(exclusions);
+        return weekMeals;
     }
+
+    //This URL was the one that worked instead of 'localhost:8080'
+    //It includes the parameters fields for timeFrame, targetCalories, diet, exclusions(empty), and my API Key
+    //https://api.spoonacular.com/mealplanner/generate?timeFrame=day&targetCalories=4000&diet=Vegan&exclude=&apiKey=c8908a61851c46d7a6706b1f5f40d8c5
 }
